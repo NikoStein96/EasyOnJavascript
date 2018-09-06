0b6efc97c6d84c7fdace1ecc3b617777c4fc92a5
Nikolai Stein <nikolai_stein@hotmail.com>
2018-09-06 10:52:58 +0200

    lololol

--- src/main/webapp/Assets/calc.js
+++ src/main/webapp/Assets/calc.js
@@ -1,10 +1,41 @@
+var button = document.querySelectorAll(".t1:not(#calculate)");
+var calcDisplay = "";
+var res = "";
 
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", addToDisplay(button[i]));
+}
 
-var button = document.querySelector(".buttons");
-console.log(button);
+var calculate = document.getElementById("calculate").addEventListener("click", doMath())
 
-button.addEventListener(onclick)
+function addToDisplay(button) {
+    return function () {
+        calcDisplay += button.innerHTML;
+        document.getElementById("display").innerHTML = calcDisplay;
+    };
+}
 
-button.click(function(event) {
- console.log("you clicked: ", this.innerHTML)
-})
+function doMath(){
+    return function () {
+        var calcDisplay = document.getElementById("display").innerHTML;
+        var finalResult = "";
+        if(calcDisplay.indexOf("+")){
+            res = calcDisplay.split("+");
+            finalResult = parseInt(res[0]) + parseInt(res[1]);
+            document.getElementById("display").innerHTML = finalResult;
+        } else if(calcDisplay.indexOf("-")){
+            res = calcDisplay.split("-");
+            console.log(res);
+            finalResult = parseInt(res[0]) - parseInt(res[1]);
+            document.getElementById("display").innerHTML = finalResult;
+        } else if(calcDisplay.indexOf("/")){
+            res = calcDisplay.split("/");
+            finalResult = parseInt(res[0]) / parseInt(res[1]);
+            document.getElementById("display").innerHTML = finalResult;
+        } else {
+            res = calcDisplay.split("*");
+            finalResult = parseInt(res[0]) * parseInt(res[1]);
+            document.getElementById("display").innerHTML = finalResult;
+        }
+    };
+}
