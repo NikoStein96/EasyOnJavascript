279c7d0970795fa2d52a17d940d727892b367022
sebastian <sebbelebbe8930@gmail.com>
2018-09-06 10:53:53 +0200

    t

---
+++ src/main/webapp/Assets/tester.js
@@ -0,0 +1,7 @@
+var texts = ["Ifølge en anonym kilde har topfolk i Det Hvide Hus indgået pagt om at modarbejde Trump, og nu vil præsidenten angiveligt gøre alt for at finde synderen", "Tidligere landsholdsanfører Daniel Agger udtrykker respekt for de 17 danske debutanter i kampen mod Slovakiet", "Den 49-årige kvinde skal i retten i dag torsdag"];
+
+function setText() {
+    var text = texts[1];
+    document.getElementById("totype").innerHTML = text;
+}
+
