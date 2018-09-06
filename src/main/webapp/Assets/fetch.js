e9cbaf192da85883a65c56f3b07b01d28f4071ef
Nikolai Stein <nikolai_stein@hotmail.com>
2018-09-06 14:28:32 +0200

    jens

--- src/main/webapp/Assets/fetch.js
+++ src/main/webapp/Assets/fetch.js
@@ -1,9 +1,16 @@
-fetch('https://jsonplaceholder.typicode.com/users/1').then(response => {
-  return response.json();
-}).then(data => {
-  // Work with JSON data here
-  var pas = data[];
-  console.log(pas[i]);
-}).catch(err => {
-  // Do something for an error here
-});
+fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then(function (response) {
+            if (response.status !== 200)
+            {
+                console.log("Problem occured... Status Code: " + response.status);
+                return;
+            }
+            response.json().then(function (data) {
+                console.log(data);
+                console.log(data.data.avatar);
+                document.getElementById("imgJson").setAttribute("src", data.data.avatar);
+            });
+        })
+        .catch(function (error) {
+            console.log("Error occured...", error);
+        });
