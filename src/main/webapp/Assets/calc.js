6ff3ec9913abed8d6daa83eadb0cd6027d1cb627
nikostein96 <nikolai_Stein@hotmail.com>
2018-09-07 13:47:17 +0200

    sebastian er manden

--- src/main/webapp/Assets/calc.js
+++ src/main/webapp/Assets/calc.js
@@ -1,41 +1,17 @@
-var button = document.querySelectorAll(".t1:not(#calculate)");
-var calcDisplay = "";
-var res = "";
+document.getElementById("buttons").addEventListener("click", appendToDisplay);
+document.getElementById("calculate").addEventListener("click", sum);
+var display = document.getElementById("display")
+display.addEventListener("click", clear);
 
-for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", addToDisplay(button[i]));
+function appendToDisplay() {
+    display.innerText += event.target.innerText;
 }
 
-var calculate = document.getElementById("calculate").addEventListener("click", doMath())
-
-function addToDisplay(button) {
-    return function () {
-        calcDisplay += button.innerHTML;
-        document.getElementById("display").innerHTML = calcDisplay;
-    };
+function sum() {
+    display.innerText = eval(display.innerText);
+    event.stopPropagation();
 }
 
-function doMath(){
-    return function () {
-        var calcDisplay = document.getElementById("display").innerHTML;
-        var finalResult = "";
-        if(calcDisplay.indexOf("+")){
-            res = calcDisplay.split("+");
-            finalResult = parseInt(res[0]) + parseInt(res[1]);
-            document.getElementById("display").innerHTML = finalResult;
-        } else if(calcDisplay.indexOf("-")){
-            res = calcDisplay.split("-");
-            console.log(res);
-            finalResult = parseInt(res[0]) - parseInt(res[1]);
-            document.getElementById("display").innerHTML = finalResult;
-        } else if(calcDisplay.indexOf("/")){
-            res = calcDisplay.split("/");
-            finalResult = parseInt(res[0]) / parseInt(res[1]);
-            document.getElementById("display").innerHTML = finalResult;
-        } else {
-            res = calcDisplay.split("*");
-            finalResult = parseInt(res[0]) * parseInt(res[1]);
-            document.getElementById("display").innerHTML = finalResult;
-        }
-    };
+function clear() {
+    display.innerText = "";
 }
--- src/main/webapp/Assets/fetch.js
+++ src/main/webapp/Assets/fetch.js
@@ -1,16 +1,53 @@
-fetch("https://jsonplaceholder.typicode.com/users/1")
-        .then(function (response) {
-            if (response.status !== 200)
-            {
-                console.log("Problem occured... Status Code: " + response.status);
-                return;
-            }
-            response.json().then(function (data) {
-                console.log(data);
-                console.log(data.data.avatar);
-                document.getElementById("imgJson").setAttribute("src", data.data.avatar);
+var s = "";
+
+document.getElementById("getuser").addEventListener("click", function () {
+    var user = document.getElementById("user").value;
+    fetch("https://jsonplaceholder.typicode.com/users/" + user)
+            .then(function (response) {
+                if (response.status !== 200)
+                {
+                    console.log("Problem occured... Status Code: " + response.status);
+                    return;
+                }
+                response.json().then(function (data) {
+                    var test = document.getElementById("data").innerHTML;
+                    var objArray = [data.name, data.phone, data.address.city, data.address.street, data.address.suite, data.address.zipcode];
+                    console.log(objArray);
+                    for (var i = 0; i < objArray.length; i++) {
+                        document.getElementById("data").innerHTML += "<p>" + objArray[i] + "</p>";
+                    }
+                });
+            })
+            .catch(function (error) {
+                console.log("Error occured...", error);
             });
-        })
-        .catch(function (error) {
-            console.log("Error occured...", error);
-        });
+})
+
+document.getElementById("getall").addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+            .then(function (response) {
+                if (response.status !== 200)
+                {
+                    console.log("Problem occured... Status Code: " + response.status);
+                    return;
+                }
+                response.json().then(function (data) {
+                    s = "<table><tr><th>Name</th><th>Phone</th></tr>";
+                    for (var i = 0; i < data.length; i++) {
+                        s += "<tr>";
+                        s += "<td>" + data[i].name + "</td>";
+                        s += "<td>" + data[i].phone + "</td>";
+                        s += "</tr>";
+                    }
+
+                    setTimeout(() => {
+                        s += "</table>";
+                         document.getElementById(data).innerHTML = s;
+                    }, 100);
+
+                });
+            })
+            .catch(function (error) {
+                console.log("Error occured...", error);
+            });
+})
